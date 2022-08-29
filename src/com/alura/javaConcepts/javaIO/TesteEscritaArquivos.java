38eee839cc55539b42b2530afdbb13f9772ba3df
Lucas Anderson Ladislau Aguiar <laanderson183@gmail.com>
2022-08-29 12:44:45 -0400

    conceitos Java IO

---
+++ src/com/alura/javaConcepts/javaIO/TesteEscritaArquivos.java
@@ -0,0 +1,20 @@
+package com.alura.javaConcepts.javaIO;
+
+import java.io.*;
+
+/**
+ * Created by Lucas Anderson Ladislau Aguiar on 29/08/2022.
+ */
+public class TesteEscritaArquivos {
+
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos = new FileOutputStream("lorem2.txt");
+        Writer osw = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        bw.write("Testando escrita de arquivos com OutputStream e write");
+        bw.newLine();
+
+        bw.close();
+    }
+}
